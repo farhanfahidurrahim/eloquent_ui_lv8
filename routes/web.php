404b9315d4135628d96effad6dbe532cabf2f60c
farhanfahidurrahim <farhan.fahidurrahim@gmail.com>
2022-12-23 11:27:47 +0600

    working

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Admin\ClassController;
+use App\Http\Controllers\Admin\StudentController;
+use App\Http\Controllers\Admin\SectionController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/password-change',[HomeController::class,'pass_change'])->name('password_change');
+Route::post('/password-update',[HomeController::class,'pass_update'])->name('password_update');
+
