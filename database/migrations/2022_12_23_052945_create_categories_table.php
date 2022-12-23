2e8ec0c32ca95252e6e13c72f3d946f05aeb755b
farhanfahidurrahim <farhan.fahidurrahim@gmail.com>
2022-12-23 11:31:50 +0600

    change

--- database/migrations/2022_12_23_052945_create_categories_table.php
+++ database/migrations/2022_12_23_052945_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTeachersTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,8 @@ class CreateTeachersTable extends Migration
      */
     public function up()
     {
-        Schema::create('teachers', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->nullable();
-            $table->string('email')->nullable();
-            $table->string('address')->nullable();
             $table->timestamps();
         });
     }
@@ -29,6 +26,6 @@ class CreateTeachersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('teachers');
+        Schema::dropIfExists('categories');
     }
 }
